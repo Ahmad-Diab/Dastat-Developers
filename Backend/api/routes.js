41ec4c0934ce7975671ad5e83bd5614506d21ffb
Omar El-Sayed <omar.elsayedmohamed@student.guc.edu.eg>
2018-03-30 08:08:18 +0200

    Adding the Backend seed

---
+++ Backend/api/routes.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+//Schema Controllers
+var User = require('./controllers/UserController');
+
+
+//please add only routers here, if you need to call a function require its class
+//DONT IMPLEMENT CONTROLLER FUNCTION HERE!!
+
+router.get('/users', User.getUsers);
+
+//exporting routes to the project
+module.exports = router;

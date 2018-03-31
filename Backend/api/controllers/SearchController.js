5dd2135b4e54f4c226ea70d5962286a4c027f8ce
Ibram Medhat <ibram.medhat2@gmail.com>
2018-03-31 23:45:28 +0200

    finishing cinema search

--- Backend/api/controllers/SearchController.js
+++ Backend/api/controllers/SearchController.js
@@ -5,10 +5,9 @@ var database = require('../config/db-connection');
 
 
 module.exports.searchByKeyWord = function(req,res,next){
-    var searchKeyN = req.params.searchKeyWord;
     var searchKeyS = req.params.searchKeyWord + '%';
-    var sql = "SELECT * FROM Cinemas WHERE number_of_halls = ? OR name LIKE ?";
-    database.query(sql, [searchKeyN,searchKeyS], function(error, result, fields){
+    var sql = "SELECT * FROM Cinemas WHERE name LIKE ? OR company LIKE ? OR location LIKE ?";
+    database.query(sql, [searchKeyS,searchKeyS,searchKeyS], function(error, result, fields){
         if(error) return next(error);
         return res.send(result);
     }); 

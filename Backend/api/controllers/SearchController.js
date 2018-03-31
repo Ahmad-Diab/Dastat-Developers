9981ad8514458052e32744179c15ce8a4200fbd5
mike13k <mikegeorge2012@gmail.com>
2018-03-31 23:35:25 +0200

    figuring how to use LIKE

--- Backend/api/controllers/SearchController.js
+++ Backend/api/controllers/SearchController.js
@@ -5,9 +5,10 @@ var database = require('../config/db-connection');
 
 
 module.exports.searchByKeyWord = function(req,res,next){
-    var where = req.params.searchKeyWord;
-    var sql = 'SELECT * FROM cinemas WHERE number_of_halls = ' + database.escape(where);
-    database.query(sql, function(error, result, fields){
+    var searchKeyN = req.params.searchKeyWord;
+    var searchKeyS = req.params.searchKeyWord + '%';
+    var sql = "SELECT * FROM Cinemas WHERE number_of_halls = ? OR name LIKE ?";
+    database.query(sql, [searchKeyN,searchKeyS], function(error, result, fields){
         if(error) return next(error);
         return res.send(result);
     }); 

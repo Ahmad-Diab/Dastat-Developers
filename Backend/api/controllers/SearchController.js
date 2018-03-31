7b941ff081076ad8314e2bc68e817d3ecfe635f2
mike13k <mikegeorge2012@gmail.com>
2018-04-01 01:36:29 +0200

    add coments to search

--- Backend/api/controllers/SearchController.js
+++ Backend/api/controllers/SearchController.js
@@ -3,10 +3,10 @@ var database = require('../config/db-connection');
 //User Controllers should be implemented here
 //DONT FORGET TO USE MODULE exports
 
-
+// Used to search in Movies or Cinemas with a key word
 module.exports.searchByKeyWord = function(req,res,next){
-    var searchKeyS = req.params.searchKeyWord + '%';
-    var searchKeyN = req.params.searchKeyWord;
+    var searchKeyS = req.params.searchKeyWord + '%';        // used in comparison to char columns using LIKE
+    var searchKeyN = req.params.searchKeyWord;              // used in comparison to int columns usin =
     var sqlMovie = 'SELECT title,genre,imagePath,year,rating FROM Movies WHERE title LIKE ? OR genre LIKE ? OR cast LIKE ? OR year = ?';
     var sqlCinema = "SELECT * FROM Cinemas WHERE name LIKE ? OR company LIKE ? OR location LIKE ?";
     var resultCinema;

2527ea241abd8f4905a3f68a7882011a6e881c8e
Ibram Medhat <ibram.medhat2@gmail.com>
2018-03-31 22:08:06 +0200

    fixing route url

--- Backend/api/controllers/SearchController.js
+++ Backend/api/controllers/SearchController.js
@@ -3,6 +3,10 @@ var database = require('../config/db-connection');
 //User Controllers should be implemented here
 //DONT FORGET TO USE MODULE exports
 
+
 module.exports.searchByKeyWord = function(req,res,next){
-    
+    database.query('select * from cinemas where name = req.params', function(error,result){
+        if(error) return next(error);
+        return res.send(result);
+    }); 
 }

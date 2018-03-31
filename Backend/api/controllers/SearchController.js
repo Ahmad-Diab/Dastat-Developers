afa0edad5f69d9d9118da5e6410806fdc4a0fe07
mike13k <mikegeorge2012@gmail.com>
2018-03-31 22:37:08 +0200

    successful conflixt fix for routes in router.get for search

--- Backend/api/controllers/SearchController.js
+++ Backend/api/controllers/SearchController.js
@@ -5,7 +5,7 @@ var database = require('../config/db-connection');
 
 
 module.exports.searchByKeyWord = function(req,res,next){
-    database.query('SELECT * FROM cinemas', function(error,result){
+    database.query('SELECT * FROM cinemas WHERE name = Mayo Movies ', function(error,result){
         if(error) return next(error);
         return res.send(result);
     }); 

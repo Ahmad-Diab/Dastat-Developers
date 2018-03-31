b3fef579bdb0053bc05dd30731a2c1302e9e82fb
Ibram Medhat <ibram.medhat2@gmail.com>
2018-03-31 21:32:53 +0200

    adding search function to the route

--- Backend/api/controllers/SearchController.js
+++ Backend/api/controllers/SearchController.js
@@ -3,3 +3,6 @@ var database = require('../config/db-connection');
 //User Controllers should be implemented here
 //DONT FORGET TO USE MODULE exports
 
+module.exports.searchByKeyWord = function(req,res,next){
+    
+}

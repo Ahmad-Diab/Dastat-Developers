126d0017f6ce6531e4aeca62e6f93613c64b6b31
mike13k <mikegeorge2012@gmail.com>
2018-04-01 00:52:30 +0200

    trying to return both objects, but failing now

--- Backend/api/controllers/SearchController.js
+++ Backend/api/controllers/SearchController.js
@@ -5,11 +5,19 @@ var database = require('../config/db-connection');
 
 
 module.exports.searchByKeyWord = function(req,res,next){
-    var searchKeyN = req.params.searchKeyWord;
     var searchKeyS = req.params.searchKeyWord + '%';
+    var searchKeyN = req.params.searchKeyWord;
     var sql = 'SELECT title,genre,imagePath,year,rating FROM Movies WHERE title LIKE ? OR genre LIKE ? OR cast LIKE ? OR year = ?';
+    var sql = "SELECT * FROM Cinemas WHERE name LIKE ? OR company LIKE ? OR location LIKE ?";
+    var resultCinema;
+    var resultMovie;
     database.query(sql, [searchKeyS,searchKeyS,searchKeyS,searchKeyN,searchKeyN], function(error, result, fields){
         if(error) return next(error);
-        return res.send(result);
-    }); 
+        resultMovie = result;
+    });
+    database.query(sql, [searchKeyS,searchKeyS,searchKeyS], function(error, result, fields){
+        if(error) return next(error);
+        resultCinema = result;
+    });
+    return res.send({"Movies": resultMovie, "Cinemas": resultCinema});
 }

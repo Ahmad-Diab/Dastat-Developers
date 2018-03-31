1c51a90e167b82158d1896a542a3198615ae3221
Ibram Medhat <ibram.medhat2@gmail.com>
2018-03-31 21:36:42 +0200

    adding comment to clarify routes

--- Backend/api/routes.js
+++ Backend/api/routes.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 //Schema Controllers
 var User = require('./controllers/UserController');
-
+var Search = require('./controllers/SearchController');
 
 //please add only routers here, if you need to call a function require its class
 //DONT IMPLEMENT CONTROLLER FUNCTION HERE!!
 
 router.get('/users', User.getUsers);
+
+
+//----------------------------------------------------Search routes--------------------------------------------//
+router.get('/search/:searchKeyWord', Search.searchByKeyWord);
 
 //exporting routes to the project
 module.exports = router;

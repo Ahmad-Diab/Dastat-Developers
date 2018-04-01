2acd31a9c9ed211455839145812808e4b41c1e71
Basem Rizk <basem.rizk@outlook.com>
2018-04-01 20:25:13 +0200

    config conflict solved

--- Backend/api/config/config.js
+++ Backend/api/config/config.js
@@ -1,8 +1,4 @@
 module.exports = {
   secret: 'aX=rr5GVhAHYl`o8U.M10<.+_YVG_nII&vg#8PlD@AkVh4(DIk|#Ip|pH}yAw5J',
-<<<<<<< HEAD
   database: 'cinema_app'
-=======
-  database: 'cinema-app',
->>>>>>> 80d71304b9d5824f70bdea812b91aeef13d27da2
 };

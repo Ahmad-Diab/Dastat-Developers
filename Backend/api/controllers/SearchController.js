5ed0914e9505c627fcd619c41bb1b6df9d0032f3
Ibram Medhat <ibram.medhat2@gmail.com>
2018-04-01 13:40:17 +0200

    adding actor to search field and enabling search by word in the middle

--- Backend/api/controllers/SearchController.js
+++ Backend/api/controllers/SearchController.js
@@ -5,15 +5,19 @@ var database = require('../config/db-connection');
 
 // Used to search in Movies or Cinemas with a key word
 module.exports.searchByKeyWord = function(req,res,next){
-    var searchKeyS = req.params.searchKeyWord + '%';        // used in comparison to char columns using LIKE
+    var searchKeyS = '%' +  req.params.searchKeyWord + '%';        // used in comparison to char columns using LIKE
     var searchKeyN = req.params.searchKeyWord;              // used in comparison to int columns usin =
     var sqlMovie = 'SELECT title,genre,imagePath,year,rating FROM Movies WHERE title LIKE ? OR genre LIKE ? OR cast LIKE ? OR year = ?';
-    var sqlCinema = "SELECT * FROM Cinemas WHERE name LIKE ? OR company LIKE ? OR location LIKE ?";
+    var sqlCinema = 'SELECT * FROM Cinemas WHERE name LIKE ? OR company LIKE ? OR location LIKE ?';
+    var sqlActor = 'SELECT name FROM Actors WHERE name LIKE ?'
     database.query(sqlMovie, [searchKeyS,searchKeyS,searchKeyS,searchKeyN,searchKeyN], function(error, movieResult, fields){
         if(error) return next(error);
         database.query(sqlCinema, [searchKeyS,searchKeyS,searchKeyS], function(error, cinemaResult, fields){
             if(error) return next(error);
-            return res.send({"Movies": movieResult,"Cinemas": cinemaResult});
+            database.query(sqlActor, [searchKeyS] , function(error, actorResult, fields){
+                if(error) return next(error);
+            return res.send({"Movies": movieResult,"Cinemas": cinemaResult,"Actors": actorResult});
+        });
         });
     });   
 }

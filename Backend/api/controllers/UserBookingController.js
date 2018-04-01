c4892cc1b27df1aba51fbd97a05848829df14317
Basem Rizk <basem.rizk@outlook.com>
2018-04-01 12:30:00 +0200

    makeReservation uses body instead of params

--- Backend/api/controllers/UserBookingController.js
+++ Backend/api/controllers/UserBookingController.js
@@ -24,16 +24,16 @@ module.exports.getParties = function(req, res, next){
  * @param next
  */
 module.exports.makeReservation = function(req, res, next){
-    //TODO Make a reservation based on all data need for reservation.
-    //TODO Assure valid values
+    //COMPLETED Make a reservation based on all data need for reservation.
     //TODO User-name validation needs to be done!
-    var username = req.params.username,
-        cinema_name = req.params.cinema_name,
-        cinema_location = req.params.cinema_location,
-        party_datetime = req.params.date_time,
-        hall = req.params.hall,
-        payment = req.params.payment,
-        tickets = req.params.tickets;
+
+    var username = req.body['username'],
+        cinema_name = req.body['cinema_name'],
+        cinema_location = req.body['cinema_location'],
+        party_datetime = req.body['date_time'],
+        hall = req.body['hall'],
+        payment = req.body['payment'],
+        tickets = req.body['tickets'];
 
     if(!username) {
         return res.status(422).json({
@@ -87,7 +87,7 @@ module.exports.makeReservation = function(req, res, next){
             '('+username+','+payment+','+seatNum+','+party_datetime+','+hall+','+
             cinema_location+','+cinema_name+');';
 
-        database.query(sqlInsertionIntoTicket, function (error, results) {
+        database.query(sqlInsertionIntoTicket, function (error) {
             if(error){
                 return next(error);
             }
@@ -95,7 +95,7 @@ module.exports.makeReservation = function(req, res, next){
             res.status(200).json({
                 err: null,
                 msg: 'Booking Request has been completed successfully.',
-                data: req
+                data: req.body
             });
         });
     }

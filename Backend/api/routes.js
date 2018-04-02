7b46ef20b6505d09781e06552f4715bc1ea8dcf8
Basem Rizk <basem.rizk@outlook.com>
2018-04-02 23:09:34 +0200

    makeReservaton in user.service

--- Backend/api/routes.js
+++ Backend/api/routes.js
@@ -6,26 +6,22 @@ var User = require('./controllers/UserController'),
     UserBooking = require('./controllers/UserBookingController');
 
 //please add only routers here, if you need to call a function require its class
-//DONT IMPLEMENT CONTROLLER FUNCTION HERE!!
+//DON'T IMPLEMENT CONTROLLER FUNCTION HERE!!
 
 router.get('/users', User.getUsers);
-router.get('/userBooking/getParties/:cinemaName/:movieName/:date', UserBooking.getParties);
-
 
 //-------------------------------User Booking Routes---------------------------------
 //TODO Authentication before booking
-router.get('/userBooking/getParties/:cinemaName/:movieName/:date', UserBooking.getParties);
 router.post('/userBooking/makeReservation',UserBooking.makeReservation);
 router.post('/userBooking/usePromoCode', UserBooking.usePromoCode);
+router.get('/userBooking/getBookings/:username', UserBooking.getBookings);
 
+//-------------------------------User fetching Routes--------------------------------
 router.get('/userBooking/getCurrentMoviesForCinema/:cinema_location/:cinema_name', UserBooking.getCurrentMoviesForCinema);
 router.get('/userBooking/getUpcomingMoviesForCinema/:cinema_location/:cinema_name', UserBooking.getUpcomingMoviesForCinema);
 router.get('/userBooking/getCurrentMovies/', UserBooking.getCurrentMovies);
 router.get('/userBooking/getUpcomingMovies/', UserBooking.getUpcomingMovies);
-
-
-router.get('/userBooking/getBookings/:username', UserBooking.getBookings);
-
+router.get('/userBooking/getParties/:cinemaName/:movieName/:date', UserBooking.getParties);
 
 
 router.post('/test', User.test);
--- Frontend/src/app/@services/user.service.ts
+++ Frontend/src/app/@services/user.service.ts
@@ -13,4 +13,18 @@ export class UserService extends HttpService {
     return this.get('userBooking/getBookings/'+username);
    }
 
+   makeReservation(username:string, cinema_name:string, cinema_location:string, party_datetime:string,
+                   hall:string, payment:boolean ,tickets, tickets_price: number, movie_id: number) {
+    return this.post("userBooking/makeReservation", {
+      'username': username,
+      'cinema_name': cinema_name,
+      'cinema_location': cinema_location,
+      'date_time': party_datetime,
+      'hall': hall,
+      'payment': payment,
+      'tickets': tickets,
+      'price': tickets_price,
+      'movie': movie_id
+    });
+   }
 }

6026b8d0aed5018b17041cffc0be8f1b07423476
Omar El-Sayed <omar.elsayedmohamed@student.guc.edu.eg>
2018-04-03 06:24:00 +0200

    Merging Authentication with UserStories

---
+++ Frontend/src/app/@services/users.service.ts
@@ -0,0 +1,23 @@
+
+import { HttpService } from './http.service';
+import { Http } from '@angular/http';
+import { environment } from '../../environments/environment';
+import { User } from '../@objects/user'
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class UsersService extends HttpService {
+
+  constructor(public http: Http) {
+    super(http);
+  }
+
+  getUsers(){
+    return this.http.get('user/getusers');
+  }
+  
+  viewMyInfo(data){
+    return this.get('users/viewMyInfo/',data);
+  }
+  
+}

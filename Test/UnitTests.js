09bd24d8dcfdfadf5185ed5fe1a19b92202cf09d
amirZaghloul <amir.khalifa@student.guc.edu.eg>
2018-04-09 12:32:46 +0200

    initialize chai and Create first test

--- Test/UnitTests.js
+++ Test/UnitTests.js
@@ -4,13 +4,12 @@
 //PLEASE USE THE CURSOR TO MOVE TO THE LINE YOU WANT TO WRITE IN, USING THE ENTER BUTTON WILL MAKE THE SPAECES MADE IN THIS FILE USELESS
 //EVERY TIME YOU USE THE CURSOR INSTEAD OF THE ENTER BUTTON IT WILL SAVE ME A CONFLICT TO RESOLVE :)
 
-
-
-
-
-
-
-
+//Initializing Chai
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+var assert = chai.assert;
+chai.use(chaiHttp);
 
 
 
@@ -18,6 +17,23 @@
 
 
 //Amir Zaghloul
+
+describe('Seats', function() { //This should contain all realted Tests
+    it('should list the layout and booked seats on /layout/encoded GET',function(done){ //Just a Custom message to know describe what is the expected output
+        chai.request("localhost:8000").get("/api/layout/encoding").query({ //This is sending the request along with my Data that i use in the Query. use this if you send your data in req.query
+          cinema_name: 'Point 90',
+          cinema_location: 'New Cairo',
+          hall_number: '1',
+          datetime: '2018-04-01 13:00:00',
+        })
+        .end(function(req,res){
+          res.should.have.status(200); //response status should be 200 aka found
+          res.should.be.json; //response type should be json
+          assert.isArray(res.body.seats,'Booked Seats'); //the array of booked Seats has type of Array
+          done();
+        });
+    });
+  });
 
 
 

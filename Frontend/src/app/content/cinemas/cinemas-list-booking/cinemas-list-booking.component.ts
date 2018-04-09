84659399f8842cc6e050e19c869624b5d4819e0d
Steven Henry <Stevennassef97@gmail.com>
2018-04-09 13:29:12 +0200

    added Cinemas Booking List component to show in it the cinemas which show a certain movie

--- Frontend/src/app/content/cinemas/cinemas-list-booking/cinemas-list-booking.component.ts
+++ Frontend/src/app/content/cinemas/cinemas-list-booking/cinemas-list-booking.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { CinemaslistService } from '../../../@services/cinemaslist.service'
+import { BookingService } from '../../../@services/booking.service'
+import { Router } from '@angular/router';
+import {CookieService} from "angular2-cookie/services/cookies.service";
 
 @Component({
-  selector: 'app-cinemas-list-booking',
+  selector: 'app-cinemas-list',
   templateUrl: './cinemas-list-booking.component.html',
   styleUrls: ['./cinemas-list-booking.component.css']
 })
 export class CinemasListBookingComponent implements OnInit {
-
-  constructor() { }
+  cinemas;
+  movie;
+  constructor(public cinemalistService: CinemaslistService, public bookingService: BookingService,
+  public router: Router , public cookie: CookieService) { }
 
   ngOnInit() {
+    this.movie = this.cookie.getObject('movie');
+    var movieId = this.movie.movie_id;
+    this.bookingService.getMoviesForThisCinema(movieId).subscribe((response) => {
+      this.cinemas=response.data;
+      console.log(this.cinemas);
+    });
+  }
+
+  AddCinemaToCookie(cinema){
+    this.router.navigate(['booking/seating']);
+    console.log(cinema);
+    this.cookie.put("cinema" , cinema);
+
   }
 
 }

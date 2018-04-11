16b3e6d5920ba00c972b973a6d06f0f2735cc8ad
Steven Henry <Stevennassef97@gmail.com>
2018-04-11 11:53:58 +0200

    added cinemas slider in parties page to show the cienmas for the selected movie

--- Frontend/src/app/content/booking/parties/parties.component.ts
+++ Frontend/src/app/content/booking/parties/parties.component.ts
@@ -3,6 +3,7 @@ import { PartiesService } from '../../../@services/parties.service';
 import { MovieslistService } from '../../../@services/movieslist.service';
 import {Router, ActivatedRoute, Params} from '@angular/router'
 import { CookieService } from 'angular2-cookie/core';
+import { BookingService } from '../../../@services/booking.service';
 
 @Component({
   selector: 'app-parties',
@@ -12,6 +13,7 @@ import { CookieService } from 'angular2-cookie/core';
 export class PartiesComponent implements OnInit {
   parties=[];
   movies=[];
+  cinemas=[];
   selectedCinema;
   selectedMovie;
   date;
@@ -20,7 +22,8 @@ export class PartiesComponent implements OnInit {
     public router : Router,
     public route: ActivatedRoute,
     public cookie : CookieService,
-    public movieslistService: MovieslistService) { 
+    public movieslistService: MovieslistService,
+    public bookingService: BookingService) { 
 
   //   this.route.params.subscribe((params: Params )=> {
   //   this.cinemaName = params['name'];
@@ -52,11 +55,24 @@ export class PartiesComponent implements OnInit {
       
     });
   }
+  viewCinemas(){
+    this.bookingService.getMoviesForThisCinema(this.selectedMovie.movie_id).subscribe((response) => {
+      this.cinemas=response.data;
+      console.log(this.cinemas);
+    });
+  }
 
   updateMovie(movie){
     this.selectedMovie = movie;
-    
+    this.cookie.putObject('movie' , this.selectedMovie);    
+    this.viewCinemas();
   }
+
+  updateCinema(cinema){
+    this.selectedCinema = cinema;
+    this.cookie.putObject('cinema' , this.selectedCinema);    
+  }
+
   getPartiesAtThisDate(){
     this.partiesService.getParties(this.selectedCinema.location ,this.selectedCinema.name , this.selectedMovie.movie_id , this.date).subscribe((response) =>{
       this.parties = response.data;

18f45c5ab5b4f65e49e6d51071b73168d48de7cb
Omar Al-Dahrawy <omar_dahrawy2000@yahoo.com>
2018-04-14 02:41:36 +0200

    fixed getParties select query (p.date_time instead of p.date)

--- Backend/api/controllers/UserBookingController.js
+++ Backend/api/controllers/UserBookingController.js
@@ -71,7 +71,7 @@ module.exports.getParties = function(req, res){
         
     //var query = "SELECT h.cinema_name , m.title,h.hall_number , p.date , p.time , h.number_of_seats FROM Halls h JOIN Parties p ON h.hall_number = p.hall JOIN Movies m ON m.movie_id = h.movie WHERE h.cinema_name = ? AND h.cinema_location = ? AND h.movie = ? AND DATE(p.date) = ?";
     var query = 'SELECT * FROM Halls h JOIN Parties p ON h.hall_number = p.hall AND h.cinema_location = p.cinema_location AND h.cinema_name = p.cinema_name'
-    +' WHERE h.cinema_name = ? AND h.cinema_location = ? AND h.movie = ? AND DATE(p.date) = ?';
+    +' WHERE h.cinema_name = ? AND h.cinema_location = ? AND h.movie = ? AND DATE(p.date_time) = ?';
 
     console.log(cinemaName + 'getParties');
 

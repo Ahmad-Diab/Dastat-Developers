abda3b05c7627ba476809e0d7860f24b3cc87377
Ibram Medhat <ibram.medhat2@gmail.com>
2018-04-17 22:45:13 +0200

    making drop down menu show the unique values  of promocodes and cinemas

--- Admin System/src/app/promocodes/view-promocodes/view-promocodes.component.ts
+++ Admin System/src/app/promocodes/view-promocodes/view-promocodes.component.ts
@@ -10,7 +10,8 @@ export class ViewPromocodesComponent implements OnInit {
 
   promocodes = []   // array of promocodes, promocode has promocode data and in which cinema
   existPromocodes = ""  // String that when there are no promocodes, assigned to a message stating that for the admin
-
+  promocodesToShow = [] //array of unique promocodes to choose from for assigning  promocode to cinema
+  cinemasToShow = [] //array of cinemas to choose between for assigning promocode to cinema
   constructor(public promocodesService: PromocodesService) { }
 
   ngOnInit() {
@@ -20,6 +21,10 @@ export class ViewPromocodesComponent implements OnInit {
       this.promocodes = response.data;
       if(this.promocodes.length === 0)  this.existPromocodes = "No Promocodes exist"
       else  this.existPromocodes = ""
+    this.promocodesService.getPromocodesAndCinemas().subscribe((response) =>{
+      this.promocodesToShow = response.data.promocodeResults;
+      this.cinemasToShow = response.data.cinemaResults;
+    })  
     });
 
   }

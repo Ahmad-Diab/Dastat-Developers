e0a8ba3d51295f492ed88bc3e13b7982e8473016
mike13k <mikegeorge2012@gmail.com>
2018-04-17 18:44:32 +0200

    create promocodes service and import it into promocodes component

--- Admin System/src/app/promocodes/view-promocodes/view-promocodes.component.ts
+++ Admin System/src/app/promocodes/view-promocodes/view-promocodes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PromocodesService } from '../../@services/promocodes.service';
 
 @Component({
   selector: 'app-view-promocodes',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewPromocodesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public promocodesService: PromocodesService) { }
 
   ngOnInit() {
   }

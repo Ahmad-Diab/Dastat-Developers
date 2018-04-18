18344b0d6918e156d5d495497d3ccbc740cf6d68
Ibram Medhat <ibram.medhat2@gmail.com>
2018-04-18 22:07:44 +0200

    adding some comments

--- Admin System/src/app/promocodes/view-promocodes/view-promocodes.component.ts
+++ Admin System/src/app/promocodes/view-promocodes/view-promocodes.component.ts
@@ -15,8 +15,8 @@ export class ViewPromocodesComponent implements OnInit {
   existPromocodes = ""  // String that when there are no promocodes, assigned to a message stating that for the admin
   promocodesToShow = [] //array of unique promocodes to choose from for assigning  promocode to cinema
   cinemasToShow = [] //array of cinemas to choose between for assigning promocode to cinema
-  promocodeValue;
-  cinemaValue;
+  promocodeValue; //variable for ngModel
+  cinemaValue; //variable for ngModel
   promocodeActions = false    // boolean for showing/hiding actions like assign, add, edit
 
   constructor(public promocodesService: PromocodesService) { }
@@ -38,6 +38,7 @@ export class ViewPromocodesComponent implements OnInit {
 
   }
 
+    // Inserting values from the ngModel into the service 
   assignPromocodeToCinema(){
     this.promocodesService.assignPromocodeToCinema(this.promocodeValue,this.cinemaValue.split(",")[0],this.cinemaValue.split(",")[1]).subscribe((response) =>{
     })

06aca82fe7d82c8071c138153ad3e6941b7bf5a1
Basem Rizk <basem.rizk@outlook.com>
2018-04-22 20:29:02 +0200

    make-reservation typeScript outline, and added html (to be updated)

--- Admin System/src/app/admin-booking/make-reservation/make-reservation.component.ts
+++ Admin System/src/app/admin-booking/make-reservation/make-reservation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AdminTicketService} from "../../@services/admin-ticket.service";
+import {CookieService} from "angular2-cookie/core";
 
 @Component({
   selector: 'app-make-reservation',
@@ -7,11 +9,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MakeReservationComponent implements OnInit {
 
-  constructor() { }
+  adminUsername = null;
+  reserveData = null;
+  ticketIsLoaded = false;
+
+  moviesList = null; selectedMovie = null;
+  partiesDatesList = null; selectedPartyDate = null;
+  partiesTimesList = null; selectedPartyTime = null;
+
+  error = null;
+
+  constructor(public adminTicketService: AdminTicketService, public cookie: CookieService) { }
 
   ngOnInit() {
+    this.adminUsername = this.cookie.get('adminUsername');
+    this.loadMovies();
+
+    this.reserveData = {
+      username: 'mai_emad', // TODO To be changed
+      cinema_name: this.cookie.get('cinema_name'),
+      cinema_location: this.cookie.get('cinema_location'),
+      date: null,
+      time: null,
+      hall: null,
+      payment: true,
+      tickets: null,
+      ticketsNum: null,
+      price: null,
+      eachPrice: null,
+      movie: null
+    };
   }
 
+  loadMovies() {
+    //TODO get all movies in halls, using steven's function in the backend
+  }
+
+  selectMovie(event) {
+    this.selectedMovie = event.target.value;
+    this.reserveData.movie = this.selectedMovie['movie_id'];
+    this.reserveData.hall = this.selectedMovie['hall_number'];
+    this.loadPartiesDates();
+  }
+
+  loadPartiesDates() {
+
+  }
+
+  selectPartyDate(event) {
+    this.selectedPartyDate = event.target.value;
+    this.reserveData.date = this.selectedPartyDate;
+    this.loadPartiesTimes();
+  }
+
+  loadPartiesTimes() {
+
+  }
+
+  selectPartyTime(event) {
+    this.selectedPartyTime = event.target.value;
+    this.reserveData.time = this.selectedPartyTime;
+    this.loadSeatLayout();
+  }
+
+  loadSeatLayout() {
+
+  }
+
+  makeReservation() {
+
+  }
 
 
 }

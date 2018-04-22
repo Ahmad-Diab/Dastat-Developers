0c1f0bea7edd67b6b12cb12cb027c8ef811b87dd
George Maged <geo.morice@gmail.com>
2018-04-22 14:30:55 +0200

    Added Gracefull shutdown to the database backend connection.

--- Backend/api/config/db-connection.js
+++ Backend/api/config/db-connection.js
@@ -1,23 +1,30 @@
 var mysql = require('mysql');
 var config = require('./config');
 
-//mysql connection
-var connection = mysql.createConnection({
-  host     : '192.185.128.96',
-  port     : '3306',
-  user     : 'gastscou_george',
-  password : 'something',
-  database: config.database
-});
+var connection;
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+function handleDisconnect() {
+  connection = mysql.createConnection(config.database);
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
 
   console.log('successfully connected to the database on port: ' + connection.config.port + ' on thread ' + connection.threadId);
-});
+}
+
+handleDisconnect();
 
 //exporting database connection to the project
 module.exports = connection;

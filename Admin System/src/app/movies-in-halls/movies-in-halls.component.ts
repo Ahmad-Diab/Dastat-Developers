95bb029f5ac8a7107cf4df1d35d12c64502d63eb
Omar Al-Dahrawy <omar_dahrawy2000@yahoo.com>
2018-04-23 22:01:17 +0200

    Edited movies-in-halls component to get cinema name from cookie and send selected movie information in cookie to reservation page

--- Admin System/src/app/movies-in-halls/movies-in-halls.component.ts
+++ Admin System/src/app/movies-in-halls/movies-in-halls.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesInHallsService } from '../@services/movies-in-halls.service';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import {Router, ActivatedRoute} from '@angular/router';
 import { CookieService } from 'angular2-cookie/services/cookies.service';
 
 @Component({
@@ -12,19 +13,24 @@ import { CookieService } from 'angular2-cookie/services/cookies.service';
 export class MoviesInHallsComponent implements OnInit {
 
   constructor(public MoviesInHallsService : MoviesInHallsService , private modalService: NgbModal,
+    public router : Router,
     public cookie :CookieService) { }
     
     halls=[];
-    cinemas=[];
     selectedCinemaMovies=[];
-  
+    cinemas=[];
+    final = [];
+
     selectedCinema;
+    selectedMovie;
   
     editMode: boolean;
     closeResult: string;
   
   
     //==========FOR TEST===============
+    cinema_name = "Mayo Movies";
+    cinema_location = "9th of Mayo";
     username = "cinema";
     password = "lailalaila123";
     //=================================  
@@ -33,31 +39,30 @@ export class MoviesInHallsComponent implements OnInit {
 
     //======FOR TEST=======
       this.cookie.put('username', this.username);
+      this.cookie.put('cinema_name', this.cinema_name);
+      this.cookie.put('cinema_location', this.cinema_location);
     //=======FOR TEST======
-      this.editMode = false;
-      this.MoviesInHallsService.getCinemasForAdminUser(this.cookie.get('username')).subscribe((response) => {
-        this.cinemas = response.data;
-        console.log(this.cinemas);
-      })
-  
+      this.MoviesInHallsService.getAlltMoviesInCinemaForAdmin(this.cookie.get('cinema_name') , this.cookie.get('cinema_location')).subscribe((response) => {
+        this.selectedCinemaMovies = response.data;
+      });
+      this.MoviesInHallsService.getHallsForCinema(this.cookie.get('cinema_name') , this.cookie.get('cinema_location')).subscribe((response) => {
+        this.halls = response.data;
+        console.log(response.data);
+      });
+      
     }
 
-    getHalls(){
-       this.getMovies();
-       this.MoviesInHallsService.getHallsForCinema(this.selectedCinema).subscribe((response) => {
-         this.halls = response.data;
-         console.log(response.data);
-       });
-       
-     }
-   
-     getMovies(){
-       this.MoviesInHallsService.getAlltMoviesInCinemaForAdmin(this.selectedCinema.cinema_name , this.selectedCinema.cinema_location).subscribe((response) => {
-         this.selectedCinemaMovies = response.data;
-       });
-     }
-
+    finalize(){
+      this.MoviesInHallsService.getfinal(this.selectedMovie, this.cookie.get('cinema_name') , this.cookie.get('cinema_location')).subscribe((response) => {
+        this.final = response.data;
+      });
+      this.cookie.putObject('movie' , this.final);
+      console.log(this.final);
   
+      this.router.navigate(['/booking/seating']);
+    }
+  
+
   }
 
 

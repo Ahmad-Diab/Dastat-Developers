bdb2dd327b271e9a0f68ff959d644f66105afda5
George Maged <geo.morice@gmail.com>
2018-04-23 20:03:01 +0200

    Removed out-tsc folder

--- Admin System/out-tsc/app/app/docs/docs.routing.js
+++
@@ -1,9 +0,0 @@
-import { DocsComponent } from './docs.component';
-export var DocsRoutes = [{
-        path: '',
-        component: DocsComponent,
-        data: {
-            heading: 'Documentation'
-        }
-    }];
-//# sourceMappingURL=docs.routing.js.map

0875db81d2897476f906a1f2e3890f432c37240f
Basem Rizk <basem.rizk@outlook.com>
2018-04-26 21:41:16 +0200

    Some cleaning in Authentication, ctrl

--- Backend/api/controllers/Authentication.js
+++ Backend/api/controllers/Authentication.js
@@ -1,14 +1,21 @@
-var database = require('../config/db-connection');
-var config = require('../config/config');
-var bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
-var randomstring = require("randomstring");
+let database = require('../config/db-connection'),
+    config = require('../config/config'),
+    bcrypt = require('bcrypt'),
+    jwt = require('jsonwebtoken'),
+    randomstring = require("randomstring");
+
 const mailer = require("../config/nodemailer");
 
-// Authentication log in
+
+/**
+ * Authentication log in
+ * @params req, res, next
+ * @returns {*}
+ */
 module.exports.authenticate = function (req, res, next) {
-  var username = req.body.username;
-  var password = req.body.password;
+  let username = req.body.username,
+      password = req.body.password;
+
   if (!username) {
     return res.status(422).json({
       err: null,
@@ -16,6 +23,7 @@ module.exports.authenticate = function (req, res, next) {
       data: null
     });
   }
+
   if (!password) {
     return res.status(422).json({
       err: null,
@@ -23,12 +31,14 @@ module.exports.authenticate = function (req, res, next) {
       data: null
     });
   }
-  var query = 'SELECT * FROM users WHERE username = ?';
-  database.query(query, [username], function (err, results, fields) {
+
+  let query = 'SELECT * FROM users WHERE username = ?';
+
+  database.query(query, [username], function (err, results) {
     if (err) return next(err);
 
     if (results.length > 0) {
-      var user = {
+      let user = {
         username: results[0].username,
         email: results[0].email,
         phone_number: results[0].phone_number,
@@ -37,12 +47,14 @@ module.exports.authenticate = function (req, res, next) {
         age: results[0].age,
         gender: results[0].gender,
         //   active:results[0].active
-      }
-      console.log(results[0].active);
+      };
+
+      console.log(user.username + " is " + results[0].active);
+
       bcrypt.compare(password, results[0].password, function (err, isMatch) {
         if (isMatch) {
           if (results[0].active) {
-            var token = jwt.sign(user, config.secret, {
+            let token = jwt.sign(user, config.secret, {
               expiresIn: '10h'
             });
 
@@ -78,44 +90,53 @@ module.exports.authenticate = function (req, res, next) {
       });
     }
   });
-}
+};
 
 
-
-//Authentication Register
+/**
+ * Authentication Register
+ *
+ * @params req: username, and token in the body
+ * @params res, next
+ */
 module.exports.verify = function (req, res, next) {
-  var username = req.body.username;
-  var token = req.body.token;
-  var tokendb;
-  console.log(username);
-  console.log(token);
-  var query = 'SELECT * FROM users WHERE username = ?';
-  database.query(query, [username], function (err, results, fields) {
+  let username = req.body.username,
+      token = req.body.token;
+
+  //var tokendb;
+
+  console.log("Username : " + username);
+  console.log("Token : " + token);
+
+  let query = 'SELECT * FROM users WHERE username = ?';
+  database.query(query, [username], function (err, results) {
     if (err) return next(err);
 
-    if (results.length == 0) {
+    if (!results.length) {
       res.status(200).json({
         err: null,
-        msg: "Please Enter a valied Username",
+        msg: "Please Enter a valid Username",
         success: false
       });
       return;
     }
-    console.log(results[0].username);
+
+    console.log("Username found :" + results[0].username);
+
     if (results[0].active_code == token) {
-      var query = 'update users  SET active= ?, active_code=? WHERE username=?';
-      database.query(query, [true, "", username], function (err, results, fields) {
+      let query = 'update users  SET active= ?, active_code=? WHERE username=?';
+      database.query(query, [true, "", username], function (err) {
         if (err) return next(err);
+
         res.status(200).json({
           err: null,
-          msg: "Done",
+          msg: "Done, correct token",
           success: true
         });
-        return;
       });
 
     } else {
-      res.status(200).json({
+      res.status(404).json({
         err: null,
         msg: "Incorrect Token",
         success: false
@@ -123,34 +144,42 @@ module.exports.verify = function (req, res, next) {
     }
   });
 
-}
+};
+
+/**
+ *
+ * @param req
+ * @param res
+ * @param next
+ * @constructor
+ */
 module.exports.Register = function (req, res, next) {
   console.log("heree");
-  var username = req.body.username;
-  var password = req.body.password;
-  var email = req.body.email;
-  var phone_number = req.body.phone_number;
-  var credit_card = req.body.credit_card;
-  var first_name = req.body.first_name;
-  var last_name = req.body.last_name;
-  var age = req.body.age;
-  var gender = req.body.gender;
-  var active = false;
-  var active_code = randomstring.generate();;
-  var flag = true;
-  var unique = true;
-  var valid =
-    req.body.username &&
-    req.body.password &&
-    req.body.email &&
-    req.body.phone_number &&
-    req.body.first_name &&
-    req.body.last_name &&
-    req.body.age &&
-    req.body.gender;
+  let username = req.body.username,
+      password = req.body.password,
+      email = req.body.email,
+      phone_number = req.body.phone_number,
+      credit_card = req.body.credit_card,
+      first_name = req.body.first_name,
+      last_name = req.body.last_name,
+      age = req.body.age,
+      gender = req.body.gender,
+      active = false,
+      active_code = randomstring.generate(),
+      flag = true,
+      unique = true,
+      valid = req.body.username &&
+            req.body.password &&
+            req.body.email &&
+            req.body.phone_number &&
+            req.body.first_name &&
+            req.body.last_name &&
+            req.body.age &&
+            req.body.gender;
+  
+  let query = 'SELECT * FROM users WHERE username = ?';
+  database.query(query, [username], function (err, results) {
 
-  var query = 'SELECT * FROM users WHERE username = ?';
-  database.query(query, [username], function (err, results, fields) {
     if (err) return next(err);
 
     if (results.length > 0) {
@@ -162,9 +191,12 @@ module.exports.Register = function (req, res, next) {
       });
       return;
     }
-    var query = 'SELECT * FROM users WHERE email = ?';
-    database.query(query, [email], function (err, results, fields) {
+
+    let query = 'SELECT * FROM users WHERE email = ?';
+    database.query(query, [email], function (err, results) {
+
       if (err) return next(err);
+
       if (results.length > 0) {
         flag = false;
         res.status(200).json({
@@ -175,17 +207,13 @@ module.exports.Register = function (req, res, next) {
         return;
       }
 
-
-      console.log(flag + "hhhhhhhhhh");
-
-      var hashed_pass;
+      let hashed_pass;
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(req.body.password, salt, (err, hash) => {
           if (err) return next(err);
 
           hashed_pass = hash;
-
-          var user = {
+          let user = {
             username: username,
             password: hashed_pass,
             email: email,
@@ -197,23 +225,23 @@ module.exports.Register = function (req, res, next) {
             gender: gender,
             active: active,
             active_code: active_code
-          }
+          };
 
-
-          database.query('INSERT INTO users SET ?', user, function (error, results, fields) {
-            if (error) {
-
+          database.query('INSERT INTO users SET ?', user, function (error) {
+            if (error)
               return next(error);
-            }
+
             const html = '<p>Hi there,</p><br/><p>Thank you For Registering!</p><br/><p> Token:' + active_code + '</p><b></b> <br/><p>Please Write that token on that link to activate your Account.</p><a href ="http://localhost:4200/verify">here</a>';
-            mailer.sendmail(email, html)
+
+            mailer.sendmail(email, html);
 
             res.status(200).json({
               err: null,
-              msg: "Register done Please check your account to verify your account",
+              msg: "Registration done successfully," +
+              " Please check your account to verify your account.",
               success: true
             });
-            return;
+
           });
 
         });

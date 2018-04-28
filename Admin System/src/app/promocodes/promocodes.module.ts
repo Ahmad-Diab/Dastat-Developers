731771ad2d0dfdac0b2664040b23d40a8210d866
Ibram Medhat <ibram.medhat2@gmail.com>
2018-04-28 02:38:50 +0200

    adding pagination to promocode table

--- Admin System/src/app/promocodes/promocodes.module.ts
+++ Admin System/src/app/promocodes/promocodes.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { PromocodesRoutes } from './promocodes.routing';
 import { ViewPromocodesComponent } from './view-promocodes/view-promocodes.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   imports: [
+    NgxPaginationModule,
     CommonModule,
     RouterModule.forChild(PromocodesRoutes),
     FormsModule

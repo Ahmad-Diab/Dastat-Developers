b1e9201b02338aadee396b855f444b497edb4ffb
Youssef Bahaa <joe4ict@gmail.com>
2018-04-29 10:58:26 +0200

    Redirecting after sigin and changeing some front-end design

---
+++ Frontend/src/app/content/movies/actor-info/actor-info.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-actor-info",
+  templateUrl: "./actor-info.component.html",
+  styleUrls: ["./actor-info.component.scss"]
+})
+
+export class ActorInfoComponent implements OnInit {
+  
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+
+  }
+}

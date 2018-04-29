6a9baa348faef3cb936b472682268cdc42756316
mike13k <mikegeorge2012@gmail.com>
2018-04-29 03:52:21 +0200

    add pipes to promocodes and some retouch

--- Admin System/src/app/promocodes/promocodes.module.ts
+++ Admin System/src/app/promocodes/promocodes.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { PromocodesRoutes } from './promocodes.routing';
 import { ViewPromocodesComponent } from './view-promocodes/view-promocodes.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -10,6 +10,7 @@ import { NgxPaginationModule } from 'ngx-pagination';
 @NgModule({
   imports: [
     NgxPaginationModule,
+    Ng2SearchPipeModule,
     CommonModule,
     RouterModule.forChild(PromocodesRoutes),
     FormsModule

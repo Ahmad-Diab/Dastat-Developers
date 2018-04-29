4baf91f397dee52b977f366ecb883e7d1915ef64
Basem Rizk <basem.rizk@outlook.com>
2018-04-29 14:07:01 +0200

    myCinemas functions : addCinema, editCinema, deleteCinema Updated

--- Backend/api/controllers/MyCinemas.js
+++ Backend/api/controllers/MyCinemas.js
@@ -2,21 +2,99 @@
 let database = require('../config/db-connection'),
     Validations = require('../utils/validations');
 
-
+/**
+ * @param req, cinema_name, cinema_location, address, num_of_halls, is3D, is4D, company,
+ *      image_path_1 and image_path_2 in body
+ * @param res
+ * @param next
+ */
 module.exports.addCinema = function (req, res, next) {
-    let location = req.body[1],
-        address = req.body[2],
-        name = req.body[0],
-        number_of_halls = req.body[3],
-        is3D = req.body[4],
-        is4D = req.body[5],
-        company = req.body[6],
-        imagePath = req.body[7],
-        imagePath2 = req.body[8];
+    let name = req.body["cinema_name"],
+        location = req.body["cinema_location"],
+        address = req.body["address"],
+        number_of_halls = req.body["num_of_halls"],
+        is3D = req.body["is3D"],
+        is4D = req.body["is4D"],
+        company = req.body["company"],
+        imagePath = req.body["image_path_1"],
+        imagePath2 = req.body["image_path_2"];
+
+    if (!name) {
+        return res.status(422).json({
+            err: null,
+            msg: 'cinema_name is required.',
+            data: null
+        });
+    }
+    if (!location) {
+        return res.status(422).json({
+            err: null,
+            msg: 'cinema_location is required.',
+            data: null
+        });
+    }
+    if (!address) {
+        return res.status(422).json({
+            err: null,
+            msg: 'address is required.',
+            data: null
+        });
+    }
+    if (!number_of_halls) {
+        return res.status(422).json({
+            err: null,
+            msg: 'num_of_halls is required.',
+            data: null
+        });
+    }
+    if (is3D !== 1 || is3D !== 0){
+        return res.status(422).json({
+            err: null,
+            msg: 'is3D must be 0 or 1.',
+            data: null
+        });
+    }
+    if (is4D !== 1 || is4D !== 0) {
+        return res.status(422).json({
+            err: null,
+            msg: 'is4D must be 0 or 1.',
+            data: null
+        });
+    }
+    if (!company) {
+        return res.status(422).json({
+            err: null,
+            msg: 'company is required.',
+            data: null
+        });
+    }
+    if (!imagePath) {
+        return res.status(422).json({
+            err: null,
+            msg: 'image_path_1 is required.',
+            data: null
+        });
+    }
+    if (!imagePath2) {
+        return res.status(422).json({
+            err: null,
+            msg: 'image_path_2 is required.',
+            data: null
+        });
+    }
+
+    if (!Validations.isString(name) ||  !Validations.isString(location) || !Validations.isString(address) ||
+        !Validations.isNumber(number_of_halls) || !Validations.isNumber(is3D) || !Validations.isNumber(is4D)  ||
+        !Validations.isString(company) || !Validations.isString(imagePath) || !Validations.isString(imagePath2)) {
+        return res.status(422).json({
+            err: null,
+            msg: 'Provided data must be in valid types.',
+            data: null
+        });
+    }
 
     let query = 'INSERT INTO cinemas (location,address,name,number_of_halls,is3D,is4D,company,imagePath,imagePath2) VALUES (?,?,?,?,?,?,?,?,?)';
-
-    database.query(query, [location, address, name, number_of_halls, Boolean(is3D), Boolean(is4D), company, imagePath, imagePath2], function (error, results, fields) {
+    database.query(query, [location, address, name, number_of_halls, Boolean(is3D), Boolean(is4D), company, imagePath, imagePath2], function (error, results) {
         if (error) {
             return next(error);
         }
@@ -28,49 +106,110 @@ module.exports.addCinema = function (req, res, next) {
     });
 };
 
+/**
+ * @param req, cinema_name & cinema_location in params
+ *              address, num_of_halls, is3D, is4D, company, image_path_1 and image_path_2 in body
+ * @param res
+ * @param next
+ */
 module.exports.editCinema = function (req, res, next) {
-    let location = req.params['location'],
-        name = req.params['name'];
+    let name = req.params['cinema_name'],
+        location = req.params['cinema_location'];
 
-    database.query('select * from cinemas where location = ? AND name = ? ', [location, name], function (err, results) {
+    let address = req.body['address'],
+        number_of_halls = req.body['num_of_halls'],
+        is3D = req.body['is3D'],
+        is4D = req.body['is4D'],
+        company = req.body['company'],
+        imagePath = req.body['image_path_1'],
+        imagePath2 = req.body['image_path_2'];
+
+    if (!name) {
+        return res.status(422).json({
+            err: null,
+            msg: 'cinema_name is required.',
+            data: null
+        });
+    }
+    if (!location) {
+        return res.status(422).json({
+            err: null,
+            msg: 'cinema_location is required.',
+            data: null
+        });
+    }
+    if (!address) {
+        return res.status(422).json({
+            err: null,
+            msg: 'address is required.',
+            data: null
+        });
+    }
+    if (!number_of_halls) {
+        return res.status(422).json({
+            err: null,
+            msg: 'num_of_halls is required.',
+            data: null
+        });
+    }
+    if (is3D !== 1 || is3D !== 0){
+        return res.status(422).json({
+            err: null,
+            msg: 'is3D must be 0 or 1.',
+            data: null
+        });
+    }
+    if (is4D !== 1 || is4D !== 0) {
+        return res.status(422).json({
+            err: null,
+            msg: 'is4D must be 0 or 1.',
+            data: null
+        });
+    }
+    if (!company) {
+        return res.status(422).json({
+            err: null,
+            msg: 'company is required.',
+            data: null
+        });
+    }
+    if (!imagePath) {
+        return res.status(422).json({
+            err: null,
+            msg: 'image_path_1 is required.',
+            data: null
+        });
+    }
+    if (!imagePath2) {
+        return res.status(422).json({
+            err: null,
+            msg: 'image_path_2 is required.',
+            data: null
+        });
+    }
+
+
+    if (!Validations.isString(name) ||  !Validations.isString(location) || !Validations.isString(address) ||
+        !Validations.isNumber(number_of_halls) || !Validations.isNumber(is3D) || !Validations.isNumber(is4D)  ||
+        !Validations.isString(company) || !Validations.isString(imagePath) || !Validations.isString(imagePath2)) {
+        return res.status(422).json({
+            err: null,
+            msg: 'Provided data must be in valid types.',
+            data: null
+        });
+    }
+
+    database.query('SELECT * FROM cinemas WHERE location = ? AND name = ? ', [location, name], function (err, results) {
         if (err) return next(err);
 
-        let address = req.body.address,
-            number_of_halls = req.body.number_of_halls,
-            is3D = req.body.is3D,
-            is4D = req.body.is4D,
-            company = req.body.company,
-            imagePath = req.body.imagePath,
-            imagePath2 = req.body.imagePath2;
+        database.query('UPDATE cinemas SET address = ?, number_of_halls = ?, is3D = ? , is4D = ? , company = ? , imagePath = ?, imagePath2 = ? where location = ? and name = ? ', [address, number_of_halls, Boolean(is3D), Boolean(is4D), company, imagePath, imagePath2, location, name],
+            function (err, results) {
 
-        if (!address) {
-            address = results[0].address;
-        }
-        if (!number_of_halls) {
-            number_of_halls = results[0].number_of_halls;
-        }
-        if (!is3D) {
-            is3D = results[0].is3D;
-        }
-        if (!is4D) {
-            is4D = results[0].is4D;
-        }
-
-        if (!company) {
-            company = results[0].company;
-        }
-        if (!imagePath) {
-            imagePath = results[0].imagePath;
-        }
-        if (!imagePath2) {
-            imagePath2 = results[0].imagePath2;
-        }
-
-        database.query('UPDATE cinemas SET address = ?, number_of_halls = ?, is3D = ? , is4D = ? , company = ? , imagePath = ?, imagePath2 = ? where location = ? and name = ? ', [address, number_of_halls, Boolean(is3D), Boolean(is4D), company, imagePath, imagePath2, location, name], function (err, results, fields) {
             if (err) return next(err);
+
             return res.send({
                 err: null,
-                msg: null,
+                msg: "Cinema is edited successfully",
                 data: results
             });
         });

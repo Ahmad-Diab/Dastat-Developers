66ab298d679cc1d990cdfbd0ae2eb0c83873ff5f
George Maged <geo.morice@gmail.com>
2018-05-25 06:34:22 +0200

    fix - fixed cinema info page (movies in a cinema still not working properly)

--- Frontend/src/app/content/cinemas/cinema-info/cinema-info.component.ts
+++ Frontend/src/app/content/cinemas/cinema-info/cinema-info.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {CinemaInfoService} from '../../../@services/cinema-info.service';
-import {Router, ActivatedRoute, Params} from '@angular/router'
+import { CinemaInfoService } from '../../../@services/cinema-info.service';
+import { Router, ActivatedRoute, Params } from '@angular/router'
 import { CinemaslistService } from '../../../@services/cinemaslist.service'
 import { CinemasListComponent } from '../cinemas-list/cinemas-list.component'
 import { CookieService } from 'angular2-cookie/core';
@@ -12,54 +12,51 @@ import { CookieService } from 'angular2-cookie/core';
 })
 export class CinemaInfoComponent implements OnInit {
   cinema;
-   name;
-   location;
-   moviesInCinema;
-   chooseDate;
-   
+  name;
+  location;
+  moviesInCinema;
+  chooseDate;
+
 
 
   constructor(public cinemaSerive: CinemaInfoService,
-  public router : Router,
-  public route: ActivatedRoute,
-  public cookie: CookieService) {
+    public router: Router,
+    public route: ActivatedRoute,
+    public cookie: CookieService) {
 
-    this.route.params.subscribe((params: Params )=> {
+    this.route.params.subscribe((params: Params) => {
       this.name = params['name'];
       this.location = params['location'];
     });
 
-   }
-   
+  }
+
   ngOnInit() {
-    document.getElementById("moviesTab").click();
     var data = {
-     cinema:this.name,
-     location:this.location,
-     
+      cinema: this.name,
+      location: this.location,
+
     }
 
-       
-
-    
-    this.cinemaSerive.getCinemaInfo(data.cinema,data.location).subscribe((response) => {
-      this.cinema=response;
+    this.cinemaSerive.getCinemaInfo(data.cinema, data.location).subscribe((response) => {
+      this.cinema = response.data[0];
     });
 
-    this.cinemaSerive.getMoviesInCinema(data.cinema,data.location).subscribe((response) => {   
-      this.moviesInCinema=response;
+    this.cinemaSerive.getMoviesInCinema(data.cinema, data.location).subscribe((response) => {
+      this.moviesInCinema = response.data;
+      console.log(this.moviesInCinema);
     });
 
   }
 
-  GoToReservation(movie){
-    this.cookie.putObject('movie' , movie);
+  GoToReservation(movie) {
+    this.cookie.putObject('movie', movie);
     console.log(this.cinema);
   }
 
-  filterBy(sorting_item,searchValue){
-    if(sorting_item != undefined && searchValue != undefined) this.router.navigate(['cinemas/list',sorting_item,searchValue]);
+  filterBy(sorting_item, searchValue) {
+    if (sorting_item != undefined && searchValue != undefined) this.router.navigate(['cinemas/list', sorting_item, searchValue]);
     else this.router.navigate(['cinemas/list']);
-}
+  }
 
 }
